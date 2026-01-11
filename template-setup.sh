0dffbb976e0313bbc7835d1f2e9de3b77f821673
Tim Palmer <tpalmerstudios@gmail.com>
2026-01-11 15:28:13 -0600

    close #1. Program runs ninja and make and tests output

--- template-setup.sh
+++ template-setup.sh
@@ -28,13 +28,14 @@ cp_check ./CMakeLists.txt
 cp_check ./CONTRIBUTING.md
 cp_check ./Doxyfile
 cp_check ./LICENSE
+cp_check ./test.sh
 
 cp ./CHANGELOG-NEW.md ../$proj_command/CHANGELOG.md
 cp ./README-NEW.md ../$proj_command/README.md
 
 cd ../$proj_command
 
-# find and replace recursively through new current directory
+# fnd and replace recursively through new current directory
 read -p "Project Name in a readable format: " proj_readable
 read -p "Project description. 1-2 Sentences don't use \\ or \/ or \|: " proj_description
 read -p "Project Command in Upper Case (used for CMake file globbing): " proj_upper
---
+++ test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "This should be run after the basic `git init` and copy of the files to the proper directory"
+
+echo "${PWD##*/}"
+
+read -p "Type the project name. Must match the Current Directory: " input_dir
+
+if [[ "$input_dir" != "${PWD##*/}" ]] then
+	echo "Wrong Directory! Exiting"
+	exit 1
+fi
+
+cmake -G Ninja -B build --log-level=ERROR || exit 1
+ninja -C build || exit 1
+rm -rf build/*
+
+cmake -B build --log-level=ERROR || exit 1
+cd build
+make -s build_all || exit 1
+
+cd ..
+rm -rf build/
+rm -rf docs/
+echo "Ninja Built successfuly"
+echo "make Built successfuly"
+echo "build & docs directories removed"
+
