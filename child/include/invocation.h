9c04782f70acba539e23c122e19c441d5505b7ac
Rushabh Jain <jainrushabh1801@gmail.com>
2026-02-15 11:22:53 +0530

    Initialize invocation module

---
+++ child/include/invocation.h
@@ -0,0 +1,16 @@
+/**
+ * @file: invocation.h
+ * @brief: Header file for invocation modile
+ */
+
+/*
+ * project: 01PROJTEMP
+ * module: sys_info
+ * created: 2026-02-15
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * author: Rushabh Jain
+ */
+#ifndef INVOCATION_H
+#define INVOCATION_H
+
+#endif // INVOCATION_H
---
+++ child/src/invocation.c
@@ -0,0 +1,12 @@
+/**
+ * @file: invocation.c
+ * @brief: Source file for invocation modile
+ */
+
+/*
+ * project: 01PROJTEMP
+ * module: sys_info
+ * created: 2026-02-15
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * author: Rushabh Jain
+ */
