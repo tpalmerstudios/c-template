898cc564e9ef2182fbe46f856aab6bbbf6609677
Tim Palmer <tpalmerstudios@gmail.com>
2026-03-13 16:40:51 -0500

    add a basic hash map using my linked list

--- child/src/hash_map.c
+++ child/src/hash_map.c
@@ -11,11 +11,12 @@
  * author: Tim Palmer
  */
 
+#include "hash_map.h"
 #include "linked_list.h"
 #include <stdlib.h>
 #include <string.h>
 
-unsigned long
+static unsigned long
 hash (const char *str)
 {
 	unsigned long hash = 5381;
@@ -23,4 +24,36 @@ hash (const char *str)
 	while ((c = *str++))
 		hash = ((hash << 5) + hash) + c;
 	return hash;
+}
+
+ctHM_t *
+ctHMInit (size_t bucketCount)
+{
+	if (bucketCount == 0)
+		return NULL;
+	ctHM_t *map = malloc (sizeof (ctHM_t));
+	if (map == NULL)
+		return NULL;
+	map->buckets = calloc (bucketCount, sizeof (ctLL_t *));
+	if (map->buckets == NULL)
+		{
+			free (map);
+			return NULL;
+		}
+	size_t i;
+	for (i = 0; i < bucketCount; i++)
+		{
+			map->buckets[i] = ctListInit (sizeof (ctHMEntry_t));
+			if (map->buckets[i] == NULL)
+				{
+					while (i-- > 0)
+						ctListFree (map->buckets[i]);
+					free (map->buckets);
+					free (map);
+					return NULL;
+				}
+		}
+	map->bucketCount = bucketCount;
+	map->size = 0;
+	return map;
 }
